315d5b0973d91267e7b413b746a9ef921f20b61b
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-03-24 19:42:20 +0100

    stallo

---
+++ src/Resources/UserResource/Pages/CreateUser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Lucacalcaterra\FilamentLdapPlugin\Resources\UserResource\Pages;
+
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateUser extends CreateRecord
+{
+    protected static string $resource = UserResource::class;
+}
